60d7df29e4542b6f241ed382304ac7ae73d1f3a6
leco <lcosta.sp.br@gmail.com>
2019-02-27 20:23:00 -0300

    configure repository, and create bootstrap file.

--- src/main/java/guru/springfamework/Spring5MvcRestApplication.java
+++ src/main/java/guru/springfamework/Spring5MvcRestApplication.java
@@ -2,8 +2,10 @@ package guru.springfamework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "guru.springfamework.repositories")
 public class Spring5MvcRestApplication {
 
 	public static void main(String[] args) {
